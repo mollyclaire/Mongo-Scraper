10efdb91cfafdac66f9da474a738b71a7c2557c9
Molly Graham <mollyclaire.graham@gmail.com>
2019-07-24 16:03:04 -0500

    Added both models and have the server up and running.

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
-
 // Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
-// Require axios and cheerio. This makes the scraping possible
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var path = require("path");
+
+// Scraping tools
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Initialize Express
+// Requiring all models
+var db = require("./models");
+
+// Initializing the port
+var PORT = 3000;
+
+// Initializing Express
 var app = express();
 
-// Database configuration
-var databaseUrl = "artnews";
-var collections = ["scrapedData"];
+// MIDDLEWARE
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+// Using Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "/views/layouts/partials")
+}));
+app.set("view engine", "handlebars");
 
-// Handlebars
-const exphbs = require("express-handlebars");
-app.engine('handlebars', exphbs({ defaultLayout: "main", extname: '.handlebars' }));
-app.set('view engine', '.handlebars');
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
-// Listen on port 3000
-app.listen(3000, function() {
-    console.log("App running on port 3000!");
+// Starting the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
   });
