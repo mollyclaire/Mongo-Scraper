dbef0244862ee713b8d0bcf1e940430ce1df219e
Molly Graham <mollyclaire.graham@gmail.com>
2019-07-24 19:28:54 -0500

    Working on saved note function.

--- public/js/app.js
+++ public/js/app.js
@@ -10,9 +10,45 @@ $(".save").on("click", function() {
 });
 
 // Click function for "delete" (from saved) button
+$(".delete").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/delete/" + thisId
+    }).done(function(data) {
+        window.location = "/"
+    })
+});
 
 // Click function for "scrape" button
+$("#scrape").on("click", function() {
+    $.ajax({
+        method: "GET",
+        url: "/scraped",
+    }).done(function(data) {
+        console.log(data)
+        window.location = "/"
+    })
+});
 
 // Click function for clearing all articles
 
 // Click function for "note" button
+$(".save-note").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles" + thisId,
+        data: {
+          text: $("#noteText" + thisId).val()
+        }
+      }).done(function(data) {
+          // Log the response
+          console.log(data);
+          // Empty the notes section
+          $("#noteText" + thisId).val("");
+          $(".modalNote").modal("hide");
+          window.location = "/saved"
+    });
+
+});
--- server.js
+++ server.js
@@ -78,7 +78,7 @@ res.send("Scrape Complete");
 });
 
 // Displays specified saved articles
-app.get("/saved", function(req, res){
+app.get("/saved", function(req, res) {
     db.Article.find({"saved": true}).then(function(result){
         var hbsObject = { articles: result };
         res.render("saved",hbsObject);
@@ -86,12 +86,61 @@ app.get("/saved", function(req, res){
 });
 
 // Posts saved articles 
-app.post("/saved/:id", function(req, res){
+app.post("/saved/:id", function(req, res) {
     db.Article.findOneAndUpdate({"_id": req.params.id}, {"$set": {"saved": true}})
     .then(function(result) {
         res.json(result);
     }).catch(function(err){ res.json(err) });
 })
+
+// Deletes specific articles from "Saved Articles" and puts them back on the homepage
+app.post("/delete/:id", function(req, res){
+    db.Article.findOneAndUpdate({"_id": req.params.id}, {"$set": {"saved": false}})
+    .then(function(result){
+        res.json(result);
+    }).catch(function(err) { res.json(err) });
+});
+
+// Grabs a specific article by id and populates it with it's note(s)
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({"_id": req.params.id })
+      .populate("notes")
+      .then(function(result) {
+        res.json(result);
+      }).catch(function(err) { res.json(err); });
+  });
+
+// Creates an article's associated note(s)
+app.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({"_id": req.params.id }, {"notes": dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+// Clears all articles
+// app.get("/clearall", function(req, res) {
+//     db.Article.remove({})
+//     .then(function(result) {
+//         res.json(result);
+//       })
+//       .catch(function(err) {
+//         res.json(err);
+//       });
+// })
+
 
 // Starting the server
 app.listen(PORT, function() {
